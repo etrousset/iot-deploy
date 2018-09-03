278801db9eead482604fe6c548bc2bd627435871
Eric Trousset <etrousset@kaliop.com>
2018-09-03 17:22:45 +0200

    iot init: some clean up/polishing

--- README.md
+++ README.md
@@ -2,21 +2,24 @@
 
 Tools to deploy a simple IoT environment to your Kuzzle.
 
-Edit info in `config/default.json` to specify where to find your Kuzzle.
+## Configuration
 
-Launch `node k_iot_init.js` to create an iot index with base collections to handle your devices.
+Edit info in `config/default.json` to specify where to find your Kuzzle. You can create several configuration file and the script will ask for the configuration to use. This can be handy if you have several environments.
 
-This work is a early stage WIP
+Launch `node k_iot_init.js` to create an **iot** index with base collections to handle your devices.
+
+This work is an early stage WIP
 
 ## Collections
+
 | Collection | Usage |
 |------------|:----------|
-| device-state | This collection stores the state of the devices |
-| device-info | This collection is where to store static information about the device (user it belongs to, current firmware version, friendly name...) |
-| firmware-update | This collection allow handling firmware updates for devices |
+| **device-state** | This collection stores the state of the devices |
+| **device-info** | This collection is where to store static information about the device (user it belongs to, current firmware version, friendly name...) |
+| **firmware-update** | This collection allow handling firmware updates for devices |
 
+### device-state mapping
 
-### Device state mapping
 ```javascript
 {
   "device_id" : "string_uid",          // an UID that identify the device the state represents
--- k_iot_init.js
+++ k_iot_init.js
@@ -1,63 +1,112 @@
-var Kuzzle = require('kuzzle-sdk') 
-var fs = require('fs')
-var config = require('./config/default.json')
+var Kuzzle = require('kuzzle-sdk')
+var Promise = require("bluebird");
+var fs = Promise.promisifyAll(require("fs"))
+var Enquirer = require('enquirer')
+var enquirer = new Enquirer()
 
-var kuzzle_cfg = config.kuzzle
+enquirer.register('radio', require('prompt-radio'))
+enquirer.register('list', require('prompt-list'))
+enquirer.register('confirm', require('prompt-confirm'))
+enquirer.register('password', require('prompt-password'))
 
-const IOT_COLLECTIONS =
-            ['users', 'fw-updates', 'device-state', 'device-info']
+const IOT_COLLECTIONS = ['fw-updates', 'device-state', 'device-info']
 
-var kuzzle = new Kuzzle('localhost', {defaultIndex: kuzzle_cfg.index}, () => {
-  kuzzle.loginPromise('local', kuzzle_cfg.user, '1d')
-    .then(() => kuzzle.createIndexPromise(kuzzle_cfg.index))
-    .then(() => console.log(`Index '${kuzzle_cfg.index}' created...`))
-    .catch(() => console.log(`Index '${kuzzle_cfg.index}' already exists`))
-    .then(() => {
-      const promises = []
-      IOT_COLLECTIONS.forEach(col_name => {
-        promises.push(
-          kuzzle.listCollectionsPromise()
-            .then(collections => {
-              if (!collections.find(elem => elem.name === col_name)) {
+function get_configs() {
+  console.log("hello")
+  return fs.readdirAsync('config')
+}
+console.log('pif')
+get_configs()
+  .then((configs) => {
+    var config_question = [{
+      name: 'config',
+      message: 'Which configuration: ',
+      type: 'list',
+      choices: configs
+    }]
+
+    return enquirer.ask(config_question)
+  })
+  .then((resp) => {
+    var config = require('./config/' + resp.config)
+    return config.kuzzle
+  })
+  .then((kuzzle_cfg => {
+    var kuzzle = new Kuzzle(kuzzle_cfg.host, {
+      defaultIndex: kuzzle_cfg.index
+    }, () => {
+      kuzzle.loginPromise('local', kuzzle_cfg.user, '1d')
+        .catch((e) => {
+          console.log('Login with config password error: ', e.message)
+          var config_question = [{
+            name: 'password',
+            message: 'Admin password: ',
+            type: 'password',
+          }]
+
+          return enquirer.ask(config_question)
+            .then(r => {
+              return kuzzle.loginPromise('local', {
+                  username: kuzzle_cfg.user.username,
+                  password: r.password
+                },
+                '1d')
+            })
+        })
+        .catch((e) => {
+          console.log('Login error: ', e.message)
+          process.exit(-1)
+        })
+        .then(() => kuzzle.createIndexPromise(kuzzle_cfg.index))
+        .then(() => console.log(`Index '${kuzzle_cfg.index}' created...`))
+        .catch(() => console.log(`Index '${kuzzle_cfg.index}' already exists`))
+        .then(() => {
+          const promises = []
+          IOT_COLLECTIONS.forEach(col_name => {
+            promises.push(
+              kuzzle.listCollectionsPromise()
+              .then(collections => {
+                if (!collections.find(elem => elem.name === col_name)) {
+                  console.log(
+                    `[${col_name}] Collection needs to be created`)
+
+                  return kuzzle.collection(col_name).createPromise()
+                    .then(() => {
+                      console.log(`[${col_name}] Collection created`)
+                      return kuzzle.collection(col_name)
+                    })
+                } else {
+                  console.log(`[${col_name}] Collection already exists`)
+                  return kuzzle.collection(col_name)
+                }
+              })
+              .catch((err) => {
+                console.log('Failed to create collection: \n', err)
+              })
+              .then(collection => {
                 console.log(
-                  `[${col_name}] Collection needs to be created`)
+                  `[${col_name}] Setting mapping for collection`)
+                var contents = fs.readFileSync(`mappings/${col_name}.json`)
+                var jsonContent = JSON.parse(contents)
 
-                return kuzzle.collection(col_name).createPromise()
-                  .then(() => {
-                    console.log(`[${col_name}] Collection created`)
-                    return kuzzle.collection(col_name)
+                return collection.collectionMapping(jsonContent)
+                  .applyPromise()
+                  .then(() => console.log(`[${col_name}] Mapping set`))
+                  .catch(err => {
+                    console.log('/!\\ Error applying mapping:\n', err)
                   })
-              } else {
-                console.log(`[${col_name}] Collection already exists`)
-                return kuzzle.collection(col_name)
-              }
-            })
-            .catch((err) => {
-              console.log('Failed to create collection: \n', err)
-            })
-            .then(collection => {
-              console.log(
-                `[${col_name}] Setting mapping for collection`)
-              var contents = fs.readFileSync(`mappings/${col_name}.json`)
-              var jsonContent = JSON.parse(contents)
-
-              return collection.collectionMapping(jsonContent)
-                .applyPromise()
-                .then(() => console.log(`[${col_name}] Mapping set`))
-                .catch(err => {
-                  console.log('/!\\ Error applying mapping:\n', err)
-                })
-            })
-            .catch(err => {
-              console.log('Error while trying to set mapping for collection:\n', err)
-            })
-        )
-      })
-      return Promise.all(promises)
+              })
+              .catch(err => {
+                console.log('Error while trying to set mapping for collection:\n', err)
+              })
+            )
+          })
+          return Promise.all(promises)
+        })
+        .catch()
+        .then(() => {
+          kuzzle.disconnect()
+          console.log('[DONE] Your IoT environement is ready to use...')
+        })
     })
-    .catch()
-    .then(() => {
-      kuzzle.disconnect()
-      console.log('[DONE] Your IoT environement is ready to use...')
-    })
-})
+  }))
